fef45d0fe1b0640baab392d89d7ff09337fabb07
unknown <lucas_m_marques@saojose.sc.senai.br>
2019-06-17 12:26:26 -0300

    inciando a aula 3 modulo 3

---
+++ mod3/aula/3/todo.js
@@ -0,0 +1,13 @@
+var  listElement = 
+document.querySelector("#app ul");
+var inputElement =
+document.querySelector("#app input");
+var buttonElement = 
+document.querySelector("#app button")
+
+
+var todos = [
+    'fazer cafe',
+    'estudar javascript',
+    ' joga pubão'
+];
